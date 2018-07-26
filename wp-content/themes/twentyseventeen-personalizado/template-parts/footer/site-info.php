842efcbb4ea30662aec60b8cf73b981bdeee5f75
Felipe Sobral <krepper@outlook.com>
2018-07-26 19:21:49 -0300

    TEMA WORDPRESS

---
+++ wp-content/themes/twentyseventeen-personalizado/template-parts/footer/site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<span>© 2018 <a href="index.php/contato/" target="_blank">GGPixel</a>. Todos os direitos reservados.</span>
+</div><!-- .site-info -->
